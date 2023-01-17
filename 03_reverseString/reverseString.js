7efb721b2db302a5e73bf3e103a5581907716ed2
rvanderbent <46648162+rvanderbent@users.noreply.github.com>
2023-01-17 14:48:07 +0100

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let splitText = text.split('');
+    let reversedText = "";
+    for (let i = splitText.length-1; i >= 0; i--) {
+        //console.log(splitText[i]);
+        reversedText += splitText[i];
+    }
+    return reversedText;
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
