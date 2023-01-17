debb7f6bda76d365839b5ab3d72b0e22195f7bf8
rvanderbent <46648162+rvanderbent@users.noreply.github.com>
2023-01-17 16:57:09 +0100

    Finish exersice 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,13 +1,18 @@
 const removeFromArray = function(array, numberToRemove) {
 
-
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] === numberToRemove){
-            array.splice(i, 1);
+    for (let n = 1; n < arguments.length; n++){
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] === arguments[n]){
+                array.splice(i, 1);
+            }
         }
     }
+
     return array;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+//console.log(removeFromArray([1,5,3,7,],5))
--- 04_removeFromArray/removeFromArray.spec.js
+++ 04_removeFromArray/removeFromArray.spec.js
@@ -4,22 +4,22 @@ describe('removeFromArray', () => {
   test('removes a single value', () => {
     expect(removeFromArray([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
   });
-  skip('removes multiple values', () => {
+  test('removes multiple values', () => {
     expect(removeFromArray([1, 2, 3, 4], 3, 2)).toEqual([1, 4]);
   });
-  skip('ignores non present values', () => {
+  test('ignores non present values', () => {
     expect(removeFromArray([1, 2, 3, 4], 7, "tacos")).toEqual([1, 2, 3, 4]);
   });
-  skip('ignores non present values, but still works', () => {
+  test('ignores non present values, but still works', () => {
     expect(removeFromArray([1, 2, 3, 4], 7, 2)).toEqual([1, 3, 4]);
   });
-  skip('can remove all values', () => {
+  test('can remove all values', () => {
     expect(removeFromArray([1, 2, 3, 4], 1, 2, 3, 4)).toEqual([]);
   });
-  skip('works with strings', () => {
+  test('works with strings', () => {
     expect(removeFromArray(["hey", 2, 3, "ho"], "hey", 3)).toEqual([2, "ho"]);
   });
-  skip('only removes same type', () => {
+  test('only removes same type', () => {
     expect(removeFromArray([1, 2, 3], "1", 3)).toEqual([1, 2]);
   });
 });
