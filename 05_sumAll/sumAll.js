822597bb187e3ac9203a5071c35b8249d4be367c
rvanderbent <46648162+rvanderbent@users.noreply.github.com>
2023-01-20 13:06:36 +0100

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(value1, value2) {
+    let sum = 0;
+    if (value1 < 0 || value2 < 0){
+        return "ERROR";
+    }
+    if (!Number.isInteger(value1) || !Number.isInteger(value2)){
+        return "ERROR";
+    }
+    if (value1 < value2){
+        for (i = value1; i <= value2; i++){
+            sum += i;
+        }
+    }
+    else if (value1 > value2){
+        for (i = value2; i <= value1; i++){
+            sum += i;
+        }
+    }
+    else return "ERROR";
 
+    return sum;
 };
 
 // Do not edit below this line
