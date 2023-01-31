8eff6054bbcd76472ffe773f6666a9e1f5000e76
rvanderbent <46648162+rvanderbent@users.noreply.github.com>
2023-01-31 17:59:42 +0100

    Complete exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let charArray = string.toLowerCase().split('');
+    const punctuation = " .,!:;'?`".split('');
+    charArray = charArray.filter(letter => {
+        for(let i = 0; i < punctuation.length;i++){
+            if(letter ==  punctuation[i]){
+                return false;
+            }
+        }
+        return true;
+    });
 
+    for (let i = 0, j = charArray.length -1; i < Math.floor(charArray.length/2); i++,j--){
+        if(charArray[i] !== charArray[j]){
+            return false;
+        };
+    }
+    return true;
 };
 
 // Do not edit below this line
