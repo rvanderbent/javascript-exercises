9d1ac029e53d9ffcdabc07b6948c4d9778f347b9
rvanderbent <46648162+rvanderbent@users.noreply.github.com>
2023-01-31 16:41:46 +0100

    Complete exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+	return numArray.reduce((total, n) => total + n, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numArray) {
+  return numArray.reduce((total, n) => total*n, 1);
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+  let total = x;
+  for (let i = 1; i < y; i++){
+    total = total * x;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let total = 1;
+	for (let i = 1; i <= n; i++){
+    total = total * i;
+  }
+  return total;
 };
 
 // Do not edit below this line
