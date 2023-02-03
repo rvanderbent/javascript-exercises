8c3f2f1f821069eccec2f7cc42e8ee446a42729d
rvanderbent <46648162+rvanderbent@users.noreply.github.com>
2023-02-03 16:15:52 +0100

    Complete the exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
+    return people.reduce(function(oldest, current){
+  
+        let oldestAge = !oldest.yearOfDeath ? (currentYear - oldest.yearOfBirth) : (oldest.yearOfDeath - oldest.yearOfBirth);
+        let currentAge = !current.yearOfDeath ? (currentYear - current.yearOfBirth) : (current.yearOfDeath - current.yearOfBirth);
 
+
+        return currentAge > oldestAge ? current : oldest;
+
+    },people[0])
 };
 
 // Do not edit below this line
