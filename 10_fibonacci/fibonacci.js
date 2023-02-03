6ad299ffa92ac6e56519b72affe411c7c19fd2f4
rvanderbent <46648162+rvanderbent@users.noreply.github.com>
2023-02-03 14:54:51 +0100

    Complete exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (number < 0){
+        return "OOPS";
+    }
+    let last = 0;
+    let secLast = 0;
+    let fib = 1;
+    for (let i = 0; i < number; i++){
+        if (last === 0){
+            fib = 1;
+        }
+        else {
+            fib = last + secLast;
+        }
+        secLast = last;
+        last = fib;
+    }
+    return fib;
 };
 
 // Do not edit below this line
